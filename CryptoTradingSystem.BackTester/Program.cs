b77cf62ec0639a22ca087fbd0eec39b0cfc31f25
Paul Abramov <paul.abramov1911@gmail.com>
2022-06-20 10:29:08 +0200

    Created DatabaseHandler and get Data for specific indicatorType

--- CryptoTradingSystem.BackTester/Program.cs
+++ CryptoTradingSystem.BackTester/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CryptoTradingSystem.General.Data;
+using CryptoTradingSystem.General.Database.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CryptoTradingSystem.BackTester
 {
@@ -6,7 +8,13 @@ namespace CryptoTradingSystem.BackTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var connectionString = config.GetValue<string>("ConnectionString");
+
+            var databaseHandler = new MySQLDatabaseHandler(connectionString);
+
+            var test = databaseHandler.GetIndicators<EMA>(Enums.Assets.Btcusdt, Enums.TimeFrames.M5, Enums.Indicators.EMA);
         }
     }
 }
